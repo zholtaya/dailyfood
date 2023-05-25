b82fc6cb10060a4acc8cf4c2d30985eb15d2574a
tnty <tnty.working@gmail.com>
2023-05-25 19:49:58 +0300

    feat: create subcategory

--- index.php
+++ index.php
@@ -18,22 +18,22 @@ if ($_REQUEST['do'] == 'exit') {
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@200;300;400;500;600;700;800;900&display=swap"
-        rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
-    <script src="js/burger_menu.js" defer></script>
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@200;300;400;500;600;700;800;900&display=swap"
+    rel="stylesheet">
+  <link rel="stylesheet" href="css/style.css">
+  <script src="js/burger_menu.js" defer></script>
+  <title>Document</title>
 
 </head>
 
 <body>
 
-    <?php
+  <?php
     include("components/header.php");
     if (isset($_GET['page'])) {
         if ($_GET['page'] == 'sign-up') {
--- pages/add_subcategory.php
+++ pages/add_subcategory.php
@@ -1,32 +1,54 @@
+<?php
+if (isset($_POST["add_subcategory"])) {
+  $title = $_POST["name"];
+  $categoryId = $_POST["category"];
+
+  $errors = array();
+
+  if (!checkEmptyLines(array($title, $categoryId))) {
+    array_push($errors, "Заполните все поля");
+  }
+
+  if (empty($errors)) {
+    $createSubcategorySQL = "INSERT INTO subcategories (title, categoryId) VALUES ('$title', '$categoryId')";
+    $link->query($createSubcategorySQL);
+  }
+}
+?>
+
 <section class="form_admin">
-    <div class="container">
-        <div class="content_form">
-            <h3 class="title_form">
-                Добавление подкатегории
-            </h3>
-            <form method="POST" class="form_style" name="add_subcategory">
-                <div class="input_label">
-                    <label for="category" class="label_style">Выберите категорию</label>
-                    <select name="category" id="category" class="select_style">
-                        <option value="0" class="option_style">Готовая еда</option>
-                        <option value="1" class="option_style">Готовая еда</option>
-                        <option value="2" class="option_style">Готовая еда</option>
+  <div class="container">
+    <div class="content_form">
+      <h3 class="title_form">
+        Добавление подкатегории
+      </h3>
+      <form method="POST" class="form_style" name="add_subcategory">
+        <div class="input_label">
+          <label for="category" class="label_style">Выберите категорию</label>
+          <select name="category" id="category" class="select_style">
+            <?php
+            $getAllCategoriesSQL = "SELECT * FROM categories";
+            $allCategoriesResponse = $link->query($getAllCategoriesSQL);
 
-                    </select>
-
-                </div>
-
-                <div class="input_label">
-                    <label for="name" class="label_style">Введите название</label>
-                    <input type="text" class="input_style" name="name" value="<?= $name ?>" id="name">
-
-                </div>
-
-                <button class="form_button_style" type="submit" name="add_subcategory">
-                    Добавить
-                </button>
-            </form>
+            while ($category = $allCategoriesResponse->fetch_assoc()) { ?>
+              <option value="<?= $category["id"] ?>" class="option_style"><?= $category["title"] ?></option>
+            <? }
+            ?>
+          </select>
 
         </div>
+
+        <div class="input_label">
+          <label for="name" class="label_style">Введите название</label>
+          <input type="text" class="input_style" name="name" value="<?= $name ?>" id="name">
+
+        </div>
+
+        <button class="form_button_style" type="submit" name="add_subcategory">
+          Добавить
+        </button>
+      </form>
+
     </div>
+  </div>
 </section>
