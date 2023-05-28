8a047310301a6f66330efb1a2dd6eef070adb53c
tntyyy <tnty.working@gmail.com>
2023-05-28 14:16:59 +0300

    feat: cart page (layout)

--- index.php
+++ index.php
@@ -3,14 +3,14 @@ session_start();
 include("./connect/connect.php");
 include("./utils/utils.php");
 if (isset($_SESSION['uid'])) {
-  $query = "SELECT * FROM users WHERE id='{$_SESSION['uid']}'";
-  $result = $link->query($query);
-  $user = $result->fetch_assoc();
-  $id_user = $user['id'];
+    $query = "SELECT * FROM users WHERE id='{$_SESSION['uid']}'";
+    $result = $link->query($query);
+    $user = $result->fetch_assoc();
+    $id_user = $user['id'];
 }
 
 if ($_REQUEST['do'] == 'exit') {
-  session_unset();
+    session_unset();
 }
 ?>
 
@@ -18,59 +18,59 @@ if ($_REQUEST['do'] == 'exit') {
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@200;300;400;500;600;700;800;900&display=swap"
-    rel="stylesheet">
-  <link rel="stylesheet" href="css/style.css">
-  <script src="js/burger_menu.js" defer></script>
-  <script src="js/partOfTheDay.js" defer></script>
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/burger_menu.js" defer></script>
+    <script src="js/partOfTheDay.js" defer></script>
+    <title>Document</title>
 
 </head>
 
 <body>
 
     <?php
-  include("components/header.php");
-  if (isset($_GET['page'])) {
-    if ($_GET['page'] == 'sign-up') {
-      include("pages/sign_up.php");
-    }
-    if ($_GET['page'] == 'sign-in') {
-      include("pages/sign_in.php");
-    }
-    if ($_GET['page'] == 'reset-password') {
-      include("pages/reset_password.php");
-    }
-    if ($_GET['page'] == 'add-category') {
-      include("pages/add_category.php");
-    }
-    if ($_GET['page'] == 'add-subcategory') {
-      include("pages/add_subcategory.php");
-    }
-    if ($_GET['page'] == 'add-product') {
-      include("pages/add_product.php");
-    }
-    if ($_GET['page'] == 'add-recipe') {
-      include("pages/add_recipe.php");
-    }
-    if ($_GET['page'] == 'catalog') {
-      include("pages/catalog.php");
-    }
-    if ($_GET['page'] == 'product') {
-      include("pages/product.php");
-    }
-    if ($_GET['page'] == 'profile') {
-      include("pages/profile.php");
-    }
+    include("components/header.php");
+    if (isset($_GET['page'])) {
+        if ($_GET['page'] == 'sign-up') {
+            include("pages/sign_up.php");
+        }
+        if ($_GET['page'] == 'sign-in') {
+            include("pages/sign_in.php");
+        }
+        if ($_GET['page'] == 'reset-password') {
+            include("pages/reset_password.php");
+        }
+        if ($_GET['page'] == 'add-category') {
+            include("pages/add_category.php");
+        }
+        if ($_GET['page'] == 'add-subcategory') {
+            include("pages/add_subcategory.php");
+        }
+        if ($_GET['page'] == 'add-product') {
+            include("pages/add_product.php");
+        }
+        if ($_GET['page'] == 'add-recipe') {
+            include("pages/add_recipe.php");
+        }
+        if ($_GET['page'] == 'catalog') {
+            include("pages/catalog.php");
+        }
+        if ($_GET['page'] == 'product') {
+            include("pages/product.php");
+        }
+        if ($_GET['page'] == 'profile') {
+            include("pages/profile.php");
+        }
         if ($_GET['page'] == 'admin') {
             include("pages/admin.php");
         }
-        if($_GET['page']=='moderator'){
+        if ($_GET['page'] == 'moderator') {
             include("pages/moderator.php");
         }
         if ($_GET['page'] == 'moderator-recipes') {
@@ -85,13 +85,16 @@ if ($_REQUEST['do'] == 'exit') {
         if ($_GET['page'] == 'admin-products') {
             include("pages/admin_products.php");
         }
-  }
-  if (empty($_GET['page'])) {
-    include("pages/main.php");
-  }
-  include("components/footer.php");
+        if ($_GET['page'] == 'cart') {
+            include("pages/cart.php");
+        }
+    }
+    if (empty($_GET['page'])) {
+        include("pages/main.php");
+    }
+    include("components/footer.php");
 
-  ?>
+    ?>
 
 </body>
 
---
+++ pages/cart.php
@@ -0,0 +1,57 @@
+<section class="cart">
+    <div class="container">
+        <div class="header_catalog_selected_category">
+            <h2 class="headtitle_style_search">
+                Корзина
+            </h2>
+
+            <a href="#" class="delete_all_products">
+                <img src="../assets/img/icons/delete.svg" alt="">
+                <p>
+                    Очистить корзину
+                </p>
+            </a>
+        </div>
+        <div class="content_cart">
+            <div class="cart_products_list">
+                <div class="cart_product_item">
+                    <img src="../assets/img/products/product_1.png" alt="" class="image_cart_product">
+                    <div class="cart_product_item_text">
+                        <p class="cart_product_item_name">
+                            Поке с курицей
+                        </p>
+                        <p class="cart_product_item_price">
+                            340 ₽
+                        </p>
+                    </div>
+                </div>
+                <div class="changing_quantity">
+
+                </div>
+            </div>
+            <div class="content_cart_final_price">
+                <p class="final_price_title">
+                    Итого
+                </p>
+                <div class="cart_final_price_product_delivery">
+                    <div class="cart_final_price_product">
+                        <p>Товары</p>
+                        <p>680 ₽</p>
+                    </div>
+                    <div class="cart_final_price_delivery">
+                        <p>Доставка</p>
+                        <p>200 ₽</p>
+                    </div>
+                    <div class="delivery_price_counter">
+                        <p>Еще 320 ₽ до бесплатной доставки</p>
+                    </div>
+                </div>
+                <div class="total_price_cart">
+                    <p>К оплате</p>
+                    <p>1020 ₽</p>
+                </div>
+                <a href="#" class="button_cart">Перейти к оплате</a>
+            </div>
+        </div>
+    </div>
+</section>
