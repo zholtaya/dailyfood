f7ea1a3d50af93e99f1cdb40fdde8e17b1a64846
zholtaya <zholtayachashka@gmail.com>
2023-06-08 19:51:01 +0300

    feat: add recipe

--- pages/add_recipe.php
+++ pages/add_recipe.php
@@ -2,6 +2,50 @@
 if (isset($_POST["add-recipe"])) {
   $title = $_POST["name"];
   $description = $_POST["description"];
+  $time = $_POST["time"];
+  $weight = $_POST["weight"];
+
+  $selectedProductId = $_POST["selectedProductId"];
+  $calories = $_POST["calories"];
+  $proteins = $_POST["proteins"];
+  $fats = $_POST["fats"];
+  $carb = $_POST["carb"];
+  $complexity = $_POST["complexity"];
+  $step1 = $_POST["step1"];
+  $step2 = $_POST["step2"];
+  $step3 = $_POST["step3"];
+  $step4 = $_POST["step4"];
+  $step5 = $_POST["step5"];
+  $step6 = $_POST["step6"];
+
+  $banner = saveUploadedFile($_FILES["image"]);
+
+  $errors = array();
+
+  if (!checkEmptyLines(array($title, $description, $time, $weight, $selectedProductId, $calories, $proteins, $fats, $carb, $complexity, $step1))) {
+    array_push($errors, "Заполните все поля");
+  }
+
+  if (empty($errors)) {
+    $createNewRecipeSQL = "INSERT INTO recipes (title, description, weight, calories, proteins, fats, carb, banner, complexity, time, status, step1, step2, step3, step4, step5, step6) VALUES ('$title', '$description', '$weight', '$calories', '$proteins', '$fats', '$carb', '$banner', '$complexity', '$time', '1', '$step1', '$step2', '$step3', '$step4', '$step5', '$step6')";
+    $link->query($createNewRecipeSQL);
+
+    $newRecipeId = $link->insert_id;
+
+    $recipeProducts = explode(";", $selectedProductId);
+
+    if (is_array($recipeProducts)) {
+      foreach ($recipeProducts as $number) {
+        $number = trim($number);
+        $addRecipeProductSQL = "INSERT INTO recipe_products (recipeId, productId) VALUES ('$newRecipeId', '$number')";
+        $link->query($addRecipeProductSQL);
+      }
+    }
+
+    showSuccessNotification("Рецепт успешно отправлен на рассмотрение");
+  } else {
+    showErrorNotifications($errors);
+  }
 }
 ?>
 
@@ -12,7 +56,7 @@ if (isset($_POST["add-recipe"])) {
       <h3 class="title_form">
         Добавление рецепта
       </h3>
-      <form method="POST" class="form_style" name="add-recipe">
+      <form method="POST" class="form_style" name="add-recipe" enctype="multipart/form-data">
         <div class="input_label">
           <label for="name" class="label_style">Введите название</label>
           <input type="text" class="input_style" name="name" value="<?= $title ?>" id="name">
@@ -26,12 +70,15 @@ if (isset($_POST["add-recipe"])) {
         <div class="input_label">
           <label for="time" class="label_style">Введите время приготовления (в минутах)</label>
           <input type="number" class="input_style" name="time" value="<?= $time ?>" id="time">
-
+        </div>
+        <div class="input_label">
+          <label for="weight" class="label_style">Введите вес готового блюда</label>
+          <input type="number" class="input_style" name="weight" value="<?= $weight ?>" id="weight">
         </div>
         <div class="input_label">
           <label for="searchProducts" class="label_style">Выберите ингридиенты</label>
           <input type="text" class="input_style" name="searchProducts" id="search">
-          <input type="hidden" id="selectedProductId">
+          <input type="hidden" name="selectedProductId" id="selectedProductId">
           <div id="products-dropdown"></div>
         </div>
         <div class="input_group added_products"></div>
@@ -73,22 +120,21 @@ if (isset($_POST["add-recipe"])) {
           </select>
 
         </div>
-        <div class="input_label">
-          <label for="name" class="label_style">Введите шаг</label>
-          <textarea name="structure" class="input_style" id="structure" cols="20" rows="6"></textarea>
-          <button class="add_step_link">
-            Добавить шаг
-            <img src="../assets/img/icons/plus.svg" alt="">
-          </button>
+        <div id="stepsContainer">
+          <div class="input_label">
+            <label for="step1" class="label_style">Введите шаг</label>
+            <textarea name="step1" class="input_style" id="step1" cols="20" rows="6"></textarea>
+          </div>
         </div>
+        <button class="add_step_link">
+          Добавить шаг
+          <img src="../assets/img/icons/plus.svg" alt="">
+        </button>
         <div class="input_label">
           <label class="label_style">Выберите изображение</label>
           <label for="image" class="visible_input">Файл не выбран</label>
           <input type="file" class="input_style_image" name="image" value="<?= $image ?>" id="image">
-
         </div>
-
-
         <button class="form_button_style" type="submit" name="add-recipe">
           Добавить
         </button>
@@ -99,58 +145,77 @@ if (isset($_POST["add-recipe"])) {
 </section>
 
 <script>
-const searchInput = document.getElementById("search");
-const addedProductsContainer = document.querySelector(".added_products");
-const dropdown = document.getElementById("products-dropdown");
+  const stepsContainerElement = document.getElementById("stepsContainer");
+  const addStepButton = document.querySelector(".add_step_link");
+  let counter = 1;
 
-function searchProducts() {
-  dropdown.style.display = "flex";
-  dropdown.innerHTML = "";
+  addStepButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    if (counter < 6) {
+      counter++;
+      stepsContainerElement.insertAdjacentHTML("beforeend", `<div class="input_label">
+            <label for="step${counter}" class="label_style">Введите шаг</label>
+            <textarea name="step${counter}" class="input_style" id="step${counter}" cols="20" rows="6"></textarea>
+          </div>`);
+    } else {
+      addStepButton.style.display = "none";
+    }
+  })
+</script>
 
-  fetch(`actions/search_products.php?search=${searchInput.value}`)
-    .then(function(response) {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error("AJAX request failed. Status: " + response.status);
-      }
-    })
-    .then(function(products) {
-      products.forEach(function(product) {
-        const option = document.createElement("div");
-        option.innerHTML = product.name;
-        option.classList.add("products-dropdown-item")
-        option.addEventListener("click", function() {
-          document.getElementById("selectedProductId").value += `${product.id};`;
-          document.getElementById("search").value = product.name;
-          createNewProductBlock(product.id, product.name, product.firstImage);
+<script>
+  const searchInput = document.getElementById("search");
+  const addedProductsContainer = document.querySelector(".added_products");
+  const dropdown = document.getElementById("products-dropdown");
+
+  function searchProducts() {
+    dropdown.style.display = "flex";
+    dropdown.innerHTML = "";
+
+    fetch(`actions/search_products.php?search=${searchInput.value}`)
+      .then(function (response) {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("AJAX request failed. Status: " + response.status);
+        }
+      })
+      .then(function (products) {
+        products.forEach(function (product) {
+          const option = document.createElement("div");
+          option.innerHTML = product.name;
+          option.classList.add("products-dropdown-item")
+          option.addEventListener("click", function () {
+            document.getElementById("selectedProductId").value += `${product.id};`;
+            document.getElementById("search").value = product.name;
+            createNewProductBlock(product.id, product.name, product.firstImage);
+          });
+          dropdown.appendChild(option);
         });
-        dropdown.appendChild(option);
+      })
+      .catch(function (error) {
+        console.log(error);
       });
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
-}
+  }
 
-function createNewProductBlock(id, name, image) {
-  addedProductsContainer.innerHTML += `<a href="?page=product&id=${id}" class="added-product" target="_blank">
+  function createNewProductBlock(id, name, image) {
+    addedProductsContainer.innerHTML += `<a href="?page=product&id=${id}" class="added-product" target="_blank">
     <img src=${image} alt=${name} />
     <span>${name}</span>
   </a>`;
-}
-
-function hideDrowdownElement() {
-  dropdown.style.display = "none";
-}
-
-hideDrowdownElement();
-
-searchInput.addEventListener("input", () => {
-  if (searchInput.value == "") {
-    hideDrowdownElement();
-  } else {
-    searchProducts();
   }
-})
+
+  function hideDrowdownElement() {
+    dropdown.style.display = "none";
+  }
+
+  hideDrowdownElement();
+
+  searchInput.addEventListener("input", () => {
+    if (searchInput.value == "") {
+      hideDrowdownElement();
+    } else {
+      searchProducts();
+    }
+  })
 </script>
