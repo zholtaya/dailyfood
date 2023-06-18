5b02f28ef8464b4e99b855217600ed8e0838d27b
tnty <tnty.working@gmail.com>
2023-06-18 23:31:36 +0300

    fix: drawer menu fix animation & items direction

--- js/drawer.js
+++ js/drawer.js
@@ -3,56 +3,37 @@ class Drawer {
         this.drawer = document.getElementById(drawerId);
         this.isOpen = false;
         this.transitionDuration = "0.5s";
-        this.transitionTimingFunction = "ease";
-        this.viewportWidth = Math.max(
-            document.documentElement.clientWidth || 0,
-            window.innerWidth || 0
-        );
-        this.viewportHeight = Math.max(
-            document.documentElement.clientHeight || 0,
-            window.innerHeight || 0
-        );
         this.init();
     }
 
     init() {
         this.drawer.classList.add("none");
+        this.isOpen = true;
+        this.close();
     }
 
     open() {
         this.drawer.classList.remove("none");
         if (!this.isOpen) {
-            this.drawer.style.transition = `width ${this.transitionDuration} ${this.transitionTimingFunction}, height ${this.transitionDuration} ${this.transitionTimingFunction}`;
+            this.isOpen = true;
+            this.drawer.style.transition = `all ${this.transitionDuration} ease-in-out`;
             this.drawer.style.overflow = "hidden";
 
             if (this.drawer.classList.contains("left-drawer")) {
-                if (window.innerWidth <= 600) {
-                    this.drawer.style.width = `${300}px`;
-                } else {
-                    this.drawer.style.width = `${500}px`;
-                }
-            } else if (this.drawer.classList.contains("top-drawer")) {
-                this.drawer.style.height = `${this.viewportHeight}px`;
+              this.drawer.style.transform = `translateX(0px)`;
             }
-
-            // document.body.style.overflow = 'hidden';
-            this.isOpen = true;
         }
     }
 
     close() {
         this.drawer.classList.remove("none");
         if (this.isOpen) {
-            this.drawer.style.transition = `width ${this.transitionDuration} ${this.transitionTimingFunction}, height ${this.transitionDuration} ${this.transitionTimingFunction}`;
+          this.isOpen = false;
+          this.drawer.style.transition = `all ${this.transitionDuration} ease-in-out`;
 
             if (this.drawer.classList.contains("left-drawer")) {
-                this.drawer.style.width = "0px";
-            } else if (this.drawer.classList.contains("top-drawer")) {
-                this.drawer.style.height = "0px";
+              this.drawer.style.transform = `translateX(-500px)`;
             }
-
-            // document.body.style.overflow = 'auto';
-            this.isOpen = false;
         }
     }
 
@@ -64,11 +45,11 @@ class Drawer {
 document.addEventListener("DOMContentLoaded", () => {
     const mainDrawer = new Drawer("drawer");
     const openDrawerButton = document.getElementById("open-drawer-btn");
+    console.log(openDrawerButton);
     const closeDrawerButton = document.getElementById("close-drawer-btn");
 
-    // mainDrawer.open();
-
     openDrawerButton.addEventListener("click", () => {
+      console.log("click");
         mainDrawer.open();
     });
 
